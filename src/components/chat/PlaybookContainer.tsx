1a0136dae4800f1ff1856f285778d4e5148b60b2
Motin <motin@motin.eu>
2025-07-03 19:06:15 +0300

    Remove some stuff from playbook container

--- src/components/chat/PlaybookContainer.tsx
+++ src/components/chat/PlaybookContainer.tsx
@@ -110,22 +110,6 @@ export function PlaybookContainer({
                         {safeAgentState.userInfo.email}
                       </span>
                     </div>
-                    <div className="flex justify-between">
-                      <span className="text-neutral-600 dark:text-neutral-400">
-                        Credits:
-                      </span>
-                      <span className="text-neutral-900 dark:text-neutral-100 font-mono">
-                        {formatCredits(safeAgentState.userInfo.credits)}
-                      </span>
-                    </div>
-                    <div className="flex justify-between">
-                      <span className="text-neutral-600 dark:text-neutral-400">
-                        Payment:
-                      </span>
-                      <span className="text-neutral-900 dark:text-neutral-100 capitalize">
-                        {safeAgentState.userInfo.payment_method}
-                      </span>
-                    </div>
                   </div>
                 </Card>
               )}
@@ -280,54 +264,6 @@ export function PlaybookContainer({
                   </div>
                 </Card>
               )}
-
-              {/* Agent Settings */}
-              <Card className="p-4">
-                <h4 className="font-medium text-neutral-900 dark:text-neutral-100 mb-3 flex items-center gap-2">
-                  <svg
-                    className="w-4 h-4"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  Settings
-                </h4>
-                <div className="space-y-2 text-sm">
-                  <div className="flex justify-between">
-                    <span className="text-neutral-600 dark:text-neutral-400">
-                      Mode:
-                    </span>
-                    <span className="text-neutral-900 dark:text-neutral-100 capitalize">
-                      {safeAgentState.mode}
-                    </span>
-                  </div>
-                  <div className="flex justify-between">
-                    <span className="text-neutral-600 dark:text-neutral-400">
-                      Language:
-                    </span>
-                    <span className="text-neutral-900 dark:text-neutral-100 uppercase">
-                      {safeAgentState.settings?.language || "en"}
-                    </span>
-                  </div>
-                  <div className="flex justify-between">
-                    <span className="text-neutral-600 dark:text-neutral-400">
-                      Onboarding:
-                    </span>
-                    <span
-                      className={`${safeAgentState.isOnboardingComplete ? "text-green-600 dark:text-green-400" : "text-yellow-600 dark:text-yellow-400"}`}
-                    >
-                      {safeAgentState.isOnboardingComplete
-                        ? "Complete"
-                        : "In Progress"}
-                    </span>
-                  </div>
-                </div>
-              </Card>
 
               {/* Debug Info */}
               {showDebug && (
